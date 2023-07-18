01d0a1f8ecc2e2caa1e44f2793bb1aa601c5514c
kanghee <ion485@naver.com>
2023-07-18 14:37:23 +0900

    feat : autoconfiguration try

--- src/main/java/com/customext/extrestdocs/configuration/ExtenstionConfig.java
+++ src/main/java/com/customext/extrestdocs/configuration/ExtenstionConfig.java
@@ -1,12 +1,17 @@
 package com.customext.extrestdocs.configuration;
 
+import com.customext.extrestdocs.service.RestDocsExtensionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 @Configuration
+//@ConditionalOnMissionBean
 public class ExtenstionConfig {
-//    @Bean
+    @Bean
+    public RestDocsExtensionService restDocsExtensionService() {
+        return new RestDocsExtensionService();
+    }
 
 
 }
