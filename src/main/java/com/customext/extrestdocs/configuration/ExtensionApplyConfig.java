b3243091084f106afe26a7f78603a3e8c9b28b0d
kanghee <ion485@naver.com>
2023-07-21 22:29:54 +0900

    feat : test Spring Extension

--- src/main/java/com/customext/extrestdocs/configuration/ExtensionApplyConfig.java
+++ src/main/java/com/customext/extrestdocs/configuration/ExtensionApplyConfig.java
@@ -4,15 +4,18 @@ import com.customext.extrestdocs.service.RestDocsExtensionService;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Component
+//@Component
 public class ExtensionApplyConfig implements BeforeEachCallback {
 
-    @Autowired
-    RestDocsExtensionService restDocsExtensionService;
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
-        restDocsExtensionService.createExtension(context);
+        ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
+        Object mapping = applicationContext.getBean("requestMappingHandlerMapping");
+        System.out.println(mapping);
+//        restDocsExtensionService.createExtension(context);
     }
 }
